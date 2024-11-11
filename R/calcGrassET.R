4a054c22e9592f0a94cb974db0c57e6dc2cccc70
Felicitas <beier@pik-potsdam.de>
2024-11-11 12:10:33 +0100

    re-write calcGrassET for new LPJmL data (note: requires second LPJmL data update)

--- R/calcGrassET.R
+++ R/calcGrassET.R
@@ -4,10 +4,10 @@
 #'              under irrigated and rainfed conditions based on LPJmL inputs.
 #'
 #' @param selectyears   Years to be returned
-#' @param lpjml         LPJmL version required for respective inputs: natveg or crop
+#' @param lpjml         LPJmL version
 #' @param climatetype   Switch between different climate scenarios or historical baseline "GSWP3-W5E5:historical"
-#' @param season        "wholeYear":  grass et in the entire year (main + off season)
-#'                      "mainSeason": grass etP in the crop-specific growing
+#' @param season        "wholeYear":  grass ET in the entire year (main + off season)
+#'                      "mainSeason": grass ET in the crop-specific growing
 #'                                    period of LPJmL (main season)
 #'
 #' @return magpie object in cellular resolution
@@ -24,77 +24,46 @@
 
 calcGrassET <- function(selectyears, lpjml, climatetype, season) {
 
-  # Read in input data already time-smoothed and for climate scenarios harmonized to the baseline
-  if (grepl("historical", climatetype)) {
-    # Baseline is only smoothed (not harmonized)
-    stage <- "smoothed"
-  } else {
-    # Climate scenarios are harmonized to baseline
-    stage <- "harmonized2020"
-  }
-
   ####################
   ### Read in data ###
   ####################
-  # monthly irrigated grass ET
-  monthlyIrrigated <- calcOutput("LPJmL_new", subtype = "met_grass_ir",
-                                 years = selectyears,
-                                 stage = stage,
-                                 version = lpjml[["crop"]], climatetype = climatetype,
-                                 aggregate = FALSE)
-  # monthly irrigated grass ET
-  monthlyRainfed <- calcOutput("LPJmL_new", subtype = "met_grass_rf",
-                               years = selectyears,
-                               stage = stage,
-                               version = lpjml[["crop"]], climatetype = climatetype,
-                               aggregate = FALSE)
-
-
+  #### To Do (discuss with Jens): yearly grass ET (for cropsIr and for cropsRf)
+  #### To Do (discuss with Jens): from which runs? cropsIr and cropsRf or only cropsIr?
+  #### (see also: calcEvapotranspiration and calcBlueWaterConsumptionOff)
+  # irrigated grass ET in entire year
+  yearlyIrrigated <- calcOutput("LPJmLharmonize", subtype = "cropsIr:et_grass_ir",
+                                lpjmlversion = lpjml, climatetype = climatetype,
+                                aggregate = FALSE)[, selectyears, "irrigated"]
+  # rainfed grass ET in entire year
+  yearlyRainfed <- calcOutput("LPJmLharmonize", subtype = "cropsRf:et_grass_ir",
+                              lpjmlversion = lpjml, climatetype = climatetype,
+                              aggregate = FALSE)[, selectyears, "irrigated"]
   # irrigated grass ET in irrigated growing period of crop
-  grperIrrigated <- calcOutput("LPJmL_new", subtype = "cft_et_grass_ir",
-                               years = selectyears,
-                               stage = stage,
-                               version = lpjml[["crop"]], climatetype = climatetype,
-                               aggregate = FALSE)
+  grperIrrigated <- calcOutput("LPJmLharmonize", subtype = "cropsIr:cft_et_grass_ir",
+                               lpjmlversion = lpjml, climatetype = climatetype,
+                               aggregate = FALSE)[, selectyears, "irrigated"]
+
   # rainfed grass ET in rainfed growing period of crop
-  grperRainfed <- calcOutput("LPJmL_new", subtype = "cft_et_grass_rf",
-                             years = selectyears,
-                             stage = stage,
-                             version = lpjml[["crop"]], climatetype = climatetype,
-                             aggregate = FALSE)
+  grperRainfed <- calcOutput("LPJmLharmonize", subtype = "cropsRf:cft_et_grass_rf",
+                             lpjmlversion = lpjml, climatetype = climatetype,
+                             aggregate = FALSE)[, selectyears, "irrigated"]
+
+  ### @JENS: I don't need counterfactuals (i.e. irrigated grass et in rainfed growing period of crop,
+  ###        and rainfed grass et in irrigated growing period of crop) here, right?
 
   ########################
   ### Data preparation ###
   ########################
-  # Empty objects to be filled
-  grassETannual <- grassETgrper <- new.magpie(cells_and_regions = getItems(grperIrrigated, dim = 1),
-                                              years = getItems(grperIrrigated, dim = 2),
-                                              names = getItems(grperIrrigated, dim = 3),
-                                              fill = NA)
+  # Annual grass ET
+  grassETannual <- mbind(yearlyIrrigated, yearlyRainfed)
+  # Grass ET in respective growing period of crop
+  grassETgrper <- mbind(grperIrrigated, grperRainfed)
+
+  ### To Do: check ordering of dimensions and check which ordering is expected in follow-up functions
+
   # Name dimensions
   getSets(grassETannual) <- c("x", "y", "iso", "year", "crop", "irrigation")
   getSets(grassETgrper)  <- c("x", "y", "iso", "year", "crop", "irrigation")
-
-  # Extract rainfed grass ET in rainfed growing period of crop
-  grassETgrper[, , "rainfed"]   <- grperRainfed[, , "rainfed"]
-  # Extract irrigated grass ET in irrigated growing period of crop
-  grassETgrper[, , "irrigated"] <- grperIrrigated[, , "irrigated"]
-
-  ### @JENS: I don't need irrigated grass et in rainfed growing period of crop, right?
-  ### @JENS: I don't need rainfed grass et in irrigated growing period of crop, right?
-
-  ####################
-  ### Calculations ###
-  ####################
-
-  # Calculate annual rainfed grass ET
-  grassETannual[, , "rainfed"]   <- add_dimension(dimSums(monthlyRainfed,
-                                                           dim = 3),
-                                                   add = "irrigation", nm = "rainfed")
-  # Calculate annual irrigated grass ET
-  grassETannual[, , "irrigated"] <- add_dimension(dimSums(monthlyIrrigated,
-                                                           dim = 3),
-                                                   add = "irrigation", nm = "irrigated")
 
   ##############
   ### Return ###
@@ -126,8 +95,11 @@ calcGrassET <- function(selectyears, lpjml, climatetype, season) {
   if (any(out < 0)) {
     stop("calcGrassET produced negative values")
   }
+  out[out < 0] <- 0
+
   if (any((grassETannual - grassETgrper) < 0)) {
     warning("Annual grass ET < grass ET in growing period")
+    ### To Do (double-check with Jens): Are negatives ok here?
   }
 
   return(list(x            = out,
