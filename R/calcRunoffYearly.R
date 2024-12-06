50fcc8e979c1d23591ef4b219b26fe313cc6e0c6
Felicitas <beier@pik-potsdam.de>
2024-12-06 22:24:53 +0100

    bugfix related to number of years in calcRunoffYearly

--- R/calcRunoffYearly.R
+++ R/calcRunoffYearly.R
@@ -40,6 +40,7 @@ calcRunoffYearly <- function(selectyears, lpjml, climatetype) {
 
   # Precipitation/runoff on water bodies (in mio. m^3)
   runoffWater <- calcOutput("LakeFlows", subtype = "input_lake",
+                            years = selectyears,
                             lpjml = lpjml, climatetype = climatetype,
                             aggregate = FALSE)
 
