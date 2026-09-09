735dcece634b7894d17c5ddd70d521e6230f19cb
FelicitasBeier <beier@pik-potsdam.de>
2026-09-09 14:00:39 +0200

    Revert "revert toolRiverDischargeAllocation bugfix to test whether bug solved"
    This reverts commit 133b0348d350862b24c3b855d094eda7ad17e830.

--- R/toolRiverDischargeAllocation.R
+++ R/toolRiverDischargeAllocation.R
@@ -111,7 +111,7 @@ toolRiverDischargeAllocation <- function(rs, c,
         # If withdrawal constraint not fulfilled in neighbor cell:
         # jump directly to next neighbor
         if (discharge[names(n)] - prevReservedWW[names(n)] <= 0) {
-          break # To Do: this should be next (just temporary test to find bug)
+          next
         }
         # Select relevant cells
         selectCells        <- c(n, rs$downstreamcells[[n]])
