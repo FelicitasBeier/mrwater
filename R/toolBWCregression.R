373e61a93fbaf88abf8c619e4a9ed554bf2c5d7a
FelicitasBeier <beier@pik-potsdam.de>
2026-09-15 14:28:41 +0200

    Transformed BWC regression warning to note with additional information

--- R/toolBWCregression.R
+++ R/toolBWCregression.R
@@ -71,8 +71,18 @@ toolBWCregression <- function(y, x) {
   }
 
   # Check
-  mstools::toolExpectTrue(all(r2 > 0.7, na.rm = TRUE), "BWC regression has acceptable R2",
-                          level = 0, falseStatus = "warn")
+  r2min <- min(r2, na.rm = TRUE)
+  r2minWhere <- magclass::where(r2 == r2min)$true$individual
+  r2minWhere <- paste0(r2minWhere[, "year"], "/", r2minWhere[, "data"],
+                       collapse = "; ")
+
+  mstools::toolExpectTrue(
+    all(r2 > 0.7, na.rm = TRUE),
+    paste0("BWC regression has acceptable R2: minimum R2 = ",
+           round(r2min, digits = 3), " for ", r2minWhere),
+    level = 0,
+    falseStatus = "note"
+  )
 
   # Save table with regression outputs for checking
   tmp        <- as.data.frame(tmp)[, c("Region", "Data1", "Data2", "Value")]
